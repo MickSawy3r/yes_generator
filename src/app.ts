5366223edb430a4105b21a641dd38c9bb0c88da0
mohammad <mohammad.al.kalaleeb@gmail.com>
2021-03-05 22:31:17 +0200

    added dynamic package name

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import jsyaml from 'js-yaml';
 import { YesManager } from './classes_types/yes_manager';
 import { YesModule } from './classes_types/yes_module';
 import { YesRepository } from './classes_types/yes_repository';
@@ -8,15 +9,42 @@ import { YesService } from './classes_types/yes_service';
 import { YesStates } from './classes_types/yes_states';
 import { YesStateManager } from './classes_types/yes_state_manager';
 
+function createFullModule(packageName: string) {
+    const moduleName = process.argv[3];
+    if (!moduleName) {
+        throw 'Unknown module name';
+    }
+    new YesModule(packageName, moduleName);
+    new YesRoutes(packageName, moduleName);
+    new YesRepository(packageName, moduleName);
+    new YesManager(packageName, moduleName);
+    new YesService(packageName, moduleName);
+    new YesStateManager(packageName, moduleName);
+    new YesScreen(packageName, moduleName);
+    new YesStates(packageName, moduleName);
+}
+
+function getPackageName(): string {
+    const spec = jsyaml.load(fs.readFileSync('pubspec.yaml', 'utf-8'));
+    if (spec == null) {
+        throw 'Error Finding pubspec.yaml'
+    }
+
+    const s: any = spec;
+    return s['name'];
+}
+
 function main() {
-    new YesModule('barter', 'Barter');
-    new YesRoutes('barter', 'Barter');
-    new YesRepository('barter', 'Barter');
-    new YesManager('barter', 'Barter');
-    new YesService('barter', 'Barter');
-    new YesStateManager('barter', 'Barter');
-    new YesScreen('barter', 'Barter');
-    new YesStates('barter', 'Barter');
+    const packageName = getPackageName();
+
+    switch (process.argv[2]) {
+        case 'g':
+            createFullModule(packageName);
+            break;
+        default:
+            console.log('Unknown command. ' + process.argv[2]);
+            console.log('Please use g to generate modules');
+    }
 }
 
 main();
